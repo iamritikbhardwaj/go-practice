e98b433e412c3f8d6061e7d9b79478ebdd49c329
Ritik Singh <ritiklrt2@gmail.com>
2025-08-20 21:39:15 +0530

    Practiced some json handeling in go

--- main.go
+++ main.go
@@ -9,22 +9,8 @@ import (
 func main() {
 	fmt.Println("Hello World")
 	// pracPrograms.Sleeps()
-	h := &pracPrograms.MinHeap{}
-
-	h.Insert(10)
-	h.Insert(4)
-	h.Insert(15)
-	h.Insert(20)
-	h.Insert(0)
-	h.Insert(8)
-
-	fmt.Println("Heap elements:")
-	h.Print()
-	h.Remove()
-	h.Remove()
-	h.Remove()
-	h.Remove()
-	h.Remove()
-	h.Remove()
+	// h := &pracPrograms.MinHeap{}
 	// pracPrograms.SlowDown()
+	w := pracPrograms.RaffelEntry{}
+	w.RaffelWinner()
 }
--- pracPrograms/raffelWinner.go
+++ pracPrograms/raffelWinner.go
@@ -1,7 +1,38 @@
 package pracPrograms
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"os"
+)
 
-func RaffelWinner() {
-	fmt.Println("RaffelWinner")
+type RaffelEntry struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type RaffelEnties []RaffelEntry
+
+func importData() RaffelEnties {
+	data, err := os.ReadFile("entries.json")
+	if err != nil {
+		panic(err)
+	}
+
+	var entries RaffelEnties
+	json.Unmarshal(data, &entries)
+	return entries
+}
+
+func winner(entries RaffelEnties) RaffelEntry {
+	wi := rand.Intn(len(entries))
+
+	return entries[wi]
+}
+
+func (r *RaffelEntry) RaffelWinner() {
+	entries := importData()
+	winner := winner(entries)
+	fmt.Println("RaffelWinner", winner.Name)
 }
